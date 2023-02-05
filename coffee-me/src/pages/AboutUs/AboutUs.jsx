40dce53570c394a732f1a49dedcde65cea2ff477
RyCab <rcabral0805@gmail.com>
2023-02-05 18:12:41 -0500

    styling fixes

--- coffee-me/src/pages/AboutUs/AboutUs.jsx
+++ coffee-me/src/pages/AboutUs/AboutUs.jsx
@@ -9,7 +9,10 @@ export const AboutUs = () => {
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
           took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
           but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised 
-          in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop  </p>
+          in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+          and more recently with desktop but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised 
+          in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+          and more recently with desktop   </p>
         </div>
         <div className="button-group">
         <button id="ryan-button">Ryan</button>
