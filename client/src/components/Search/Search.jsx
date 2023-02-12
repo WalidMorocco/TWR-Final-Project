ca29202c2c234df2436d8d5791f9f88f18abf7aa
RyCab <rcabral0805@gmail.com>
2023-02-12 17:55:05 -0500

    Leave a review / Searchbar auto complete

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,9 +1,42 @@
 import "./styles.css"
+import { useState } from "react";
+import React from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
 
 export const Search = () => {
+
+  const [value, setValue] = useState(null);
+  console.log(value);
+
   return (
+    
     <div className="search-container">
-      <input id="search-bar" type="text" placeholder="Search..."/>
+        <GooglePlacesAutocomplete
+            selectProps={{
+              placeholder: 'Search...',
+              value,
+              onchange: setValue,
+              styles: {
+                input: (provided) => ({
+                  ...provided,
+                  color: 'blue',
+                }),
+                option: (provided) => ({
+                  ...provided,
+                  color: 'blue',
+                }),
+                singleValue: (provided) => ({
+                  ...provided,
+                  color: 'blue',
+                }),
+              },
+            }}
+            apiKey="AIzaSyBkpSZDQLvioiTKdeakMG3CQTnh5c2U0Rk"
+          />
+      {/* <input id="search-bar" type="text" placeholder="Search..."/> */}
     </div>
+    
   );
 };
+export default Search;
