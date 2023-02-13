c1645b865acb12c6b32120b291a8ec2a1ddb1a19
RyCab <rcabral0805@gmail.com>
2023-02-13 00:26:31 -0500

    style

--- client/src/components/StoreDetails/StoreDetails.jsx
+++ client/src/components/StoreDetails/StoreDetails.jsx
@@ -24,7 +24,9 @@ export const StoreDetails = () => {
 
   return (
     <div className="storeDetails-container">
-      <ImageSlider images={data.images} />
+      <div className="slider-image">
+        <ImageSlider images={data.images} />
+      </div>
       <div className="detailsTitle-container">
         <h1 id="details-title">{data.name}</h1>
       </div>
