5796fed50b0fc270dcd73f11e087275d7c230172
RyCab <rcabral0805@gmail.com>
2023-03-01 17:11:51 -0500

    style changes

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -28,6 +28,7 @@ export const Reviews = () => {
         <h2 id="lower-review">Reviews</h2>
       </div>
       <div className="leave-review-container">
+        {/* change to input */}
         <p id="review-box">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
           took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
