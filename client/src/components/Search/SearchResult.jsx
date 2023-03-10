31d5b8c934fa307a20e5681e710345e3c3d55a82
RyCab <rcabral0805@gmail.com>
2023-03-10 18:36:14 -0500

    Styling and Search

--- client/src/components/Search/SearchResult.jsx
+++ client/src/components/Search/SearchResult.jsx
@@ -1,89 +1,110 @@
 import "./styles.css";
-import { useNavigate } from "react-router-dom";
-import Car from "@mui/icons-material/LocationOnSharp";
-import Website from "@mui/icons-material/Info";
-import Phone from "@mui/icons-material/Phone";
-import useDetails from "../../hooks/useDetails";
-import { Loading } from "../Loading/Loading";
-
-export const SearchResult = ({ storeId }) => {
-  const navigate = useNavigate();
-  const { data, loading, error } = useDetails(storeId);
-
-  const refreshPage = () => {
-    window.location.reload(false);
+  import { useNavigate } from "react-router-dom";
+  import Car from "@mui/icons-material/LocationOnSharp";
+  import Website from "@mui/icons-material/Info";
+  import Phone from "@mui/icons-material/Phone";
+  import useDetails from "../../hooks/useDetails";
+  import { Loading } from "../Loading/Loading";
+  import { LocationContext } from "../../context/LocationContext";
+  import { useContext } from "react";
+  
+  const mapsBaseURL = "https://www.google.com/maps/dir/";
+  const getNavigationURL = (userlocation, storeLocation) => {
+    return `${mapsBaseURL}${
+      userlocation.location.address
+        ? userlocation.location.address
+        : "My Location"
+    }/@${userlocation.location.lat},${userlocation.location.lng}/${
+      storeLocation.address
+    }`;
   };
+  
+  export const SearchResult = ({ storeId, handleSwitchModal }) => {
+    const navigate = useNavigate();
+    const { data, loading, error } = useDetails(storeId);
+  
+    const refreshPage = () => {
+      window.location.reload(false);
+    };
+  
+    const navigateToDetails = () => {
+      navigate(`/StoreDetails/${storeId}`);
+    };
+  
+    const call = () => {
+      console.log(data.phone);
+      window.location.href = 'tel://' + data.phone;
+    }
+    
+    const locationContext = useContext(LocationContext);
+  
+    const gps = () => {
+      window.location.href = getNavigationURL(locationContext.settings, data.location);
+    }
+  
+    if (error) {
+      console.log(error);
+    }
+  
+    if (loading) {
+      return <Loading />;
+    }
 
-  const navigateToDetails = () => {
-    navigate(`/StoreDetails/${storeId}`);
+    console.log(data);
+    return (
+        <div className="results-container">
+          <div className="title-result">
+            <h1 id="r-title">{data.name}</h1>
+          </div>
+          <div className="button-search-group">
+            <button id="btnA" onClick={gps}>
+              <Car
+                style={{
+                  color: "white",
+                  fontSize: "25px",
+                  marginTop: "1px",
+                }}
+              />
+            </button>
+            <button id="btnB" onClick={navigateToDetails}>
+              <Website
+                style={{
+                  color: "white",
+                  fontSize: "25px",
+                  marginTop: "1px",
+                }}
+              />
+            </button>
+            <button id="btnC" onClick={call}>
+              <Phone
+                style={{
+                  color: "white",
+                  fontSize: "25px",
+                  marginTop: "1px",
+                }}
+              />
+            </button>
+          </div>
+          <div className="rating-result">
+            <p id="r-rating">Rating: {data.rating}</p>
+          </div>
+          <div className="delivery-result">
+            <p id="d-delivery">
+              Delivery: {data.delivery ? "Available" : "Unavailable"}
+            </p>
+          </div>
+          <div className="curbside-result">
+            <p id="c-curbside">
+              Curbside Pickup: {data.curbsidePickup ? "Available" : "Unavailable"}
+            </p>
+          </div>
+          <div className="close-button">
+            <button id="close" onClick={refreshPage}>
+              Close
+            </button>
+          </div>
+        </div>
+    );
   };
-
-  if (error) {
-    console.log(error);
-  }
-
-  if (loading) {
-    return <Loading />;
-  }
-
-  console.log(data);
-  return (
-    <div className="results-container">
-      <div className="title-result">
-        <h1 id="r-title">{data.name}</h1>
-      </div>
-      <div className="button-search-group">
-        <button id="btnA">
-          <Car
-            style={{
-              color: "white",
-              fontSize: "25px",
-              marginTop: "1px",
-            }}
-          />
-        </button>
-        <button id="btnB" onClick={navigateToDetails}>
-          <Website
-            style={{
-              color: "white",
-              fontSize: "25px",
-              marginTop: "1px",
-            }}
-          />
-        </button>
-        <button id="btnC">
-          <Phone
-            style={{
-              color: "white",
-              fontSize: "25px",
-              marginTop: "1px",
-            }}
-          />
-        </button>
-      </div>
-      <div className="open-result">
-        <p id="o-open">Hours: {data.name ? "Open" : "Closed"}</p>
-      </div>
-      <div className="rating-result">
-        <p id="r-rating">Rating: {data.rating}</p>
-      </div>
-      <div className="delivery-result">
-        <p id="d-delivery">
-          Delivery: {data.delivery ? "Available" : "Unavailable"}
-        </p>
-      </div>
-      <div className="curbside-result">
-        <p id="c-curbside">
-          Curbside Pickup: {data.curbsidePickup ? "Available" : "Unavailable"}
-        </p>
-      </div>
-      <div className="close-button">
-        <button id="close" onClick={refreshPage}>
-          Close
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export default SearchResult;
+  
+  export default SearchResult;
